b9593fb96369a40e808d4d176a528ce67b655cba
anarchytrex <anarchytrex@hotmail.com>
2021-07-15 21:00:22 +0200

    v1.2

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -14,33 +14,33 @@ export default [
     url: "https://github.com/KodeAndre/ArtGalleryProject",
     languages: "HTML, CSS and JS",
     api: "",
-    description: "I Galleri prosjektet jobbet jeg ved å lære mer om å bruke CSS til å få elementer til å virke mindre statisk på siden for å få mer inntrykk av bildene og for å gjøre at det er mindre kjedelig for de som besøker siden, jeg gjorde dette ved bruk av transforms og filters. Målet var å få artisten til å virke interessant og for å få flest folk til å besøke deres instagram, som er linket i en links del av websiden."
+    description: "In the Gallery project I worked on learning more about using CSS to make elements seem less static on the page to get more impression of the images, and to make it less boring for those who visit the page, I did this by use of transforms and filters. The goal was to make the artist seem interesting and to get a majority of people to visit their instagram, which is linked in the links section of the website."
 }, {
     id: "3",
     name: "Gallery Project upgraded to React",
     url: "https://kodeandre.github.io/artgalleryreact/",
     languages: "React",
     api: "Google Drive",
-    description: "En oppgradering av Galleri prosjektet, for en klient fra United Kingdoms, men noen få endringer i navigation og stil, samt full tilgang for artisten til å endre galleri innholdet fra en ekstern side, som er Google Drive, gjennom bruk av en iframe og embedded folder view"
+    description: "An upgrade of the Gallery project, for a client from the United Kingdoms, but a few changes in navbar and style, as well as full access for the artist to change the gallery content from an external site, being Google Drive, through the use of an iframe and embedded folder view"
 }, {
     id: "4",
     name: "Cafè Website",
     url: "https://github.com/KodeAndre/TaskCafe",
     languages: "HTML, CSS and JS",
     api: "Google Maps API",
-    description: "Denne er noe vi gjorde for å bli vant til å sette opp nettsider i restaurant forbindelse, med en navigation linje, flere sider, bilde i bakgrunn av noe på siden og konsolidering av det vi hadde lært oss til det punktet i kurset, jeg valgte i dette prosjektet å ta bruk av google maps API-en for å få et interaktivt kart som viste hvor kaféen er. Også i dette prosjektet gikk jeg inn i dark mode toggling og å endre bilde i sammenheng med dark mode togglingen."
+    description: "This is something we did to get used to setting up websites in a restaurant context, with a navbar, several pages, an image in the background of something on the page and consolidation of what we had learned up to this point in the course, I chose in this project to make use of the google maps API to get an interactive map that showed where the cafe is located. Also in this project I went into figuring out dark mode toggling and changing the image when doing dark mode toggling."
 }, {
     id: "5",
     name: "BlackJack Project",
     url: "https://github.com/KodeAndre/Blackjack",
     languages: "HTML, CSS and JS",
     api: "",
-    description: "Prosjektet ble gjort side til side med en “tutorial” fra Scrimba, som vi har brukt i kurset og i fritiden til å lære mer enn bare det kurset har tid til å gi oss, her var det for det meste JavaScript og buttons fra HTML som ble brukt til å få spillet til å virke som et ekte Blackjack spill, men med “real RNG”, noe man ikke har i skikkelig Blackjack."
+    description: "The project was done side by side with a 'tutorial' from Scrimba, which we have used in the course and in our free time to learn more than only what the course has enough time to teach us per day, this was mostly JavaScript and buttons in HTML that was used to make the game seem like a real Blackjack game, but with 'real RNG', something you do not have in real Blackjack."
 }, {
     id: "6",
     name: "Tic Tac Toe",
     url: "https://github.com/KodeAndre/test-app",
     languages: "React",
     api: "",
-    description: "Dette var mitt første møte med ReactJS i skikkeligheten og var gjort i sammenheng med å venne seg til React og hvordan det fungerer, tok et par minutter å forstå oppsettet men etter det så gikk det helt greit, veldig robust og modulært språk som virker veldig bra for å lage websider."
+    description: "This was my first meeting with ReactJS in real practice and was done in connection with getting used to React and how it works, took a few minutes to understand the setup but after that it went perfectly well, very robust and modular language that works really well to create web pages with."
 }, ]
