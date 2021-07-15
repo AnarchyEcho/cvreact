6b37b533e0d7b5c4e870b6e09c7c828f718ad97d
KodeAndre <andre.kodehode@gmail.com>
2021-07-15 10:25:32 +0200

    v0.5.3

--- src/buttons/BtnDark.js
+++ src/buttons/BtnDark.js
@@ -16,7 +16,7 @@ function BtnDark() {
       backgroundImage: "linear-gradient(0deg, #fe9000 0%, #ffb34f 100%)",
       color: "#000",
       borderRadius: "3%",
-      fontSize: "20px",
+      fontSize: "1em",
       cursor: "pointer",
       border: "2px solid #121212",
    }
