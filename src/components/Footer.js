927175f7aff2b7eb31d97f637bef586a5fac9104
anarchytrex <anarchytrex@hotmail.com>
2021-07-16 19:22:55 +0200

    v0.5.4

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,38 +5,15 @@ import ReactDOM from "react-dom"
 import Style from "../style.css"
 import Github from "../media/github/GitHub-Mark-Light-120px-plus.png"
 import LinkedIn from "../media/linkedin/LinkedInLogo.png"
-import { useTranslation } from "react-i18next";
 
-import NorFlag from "../media/Flags/norway.png"
-import EngFlag from "../media/Flags/uk.png"
+import Language from "./Language"
 
 // css to apply to all buttons that dont need to be converted in darkmode
 const style = {
     filter: "none"
 }
 
-const norBtn = {
-    border: "none",
-    marginRight: "10px",
-    backgroundColor: "#ffffff00",
-    cursor: "pointer",
-    transform: "scale(0.8)",
-    marginTop: "0",
-    paddingTop: "0",
-  }
-
-const engBtn = {
-    border: "none",
-    backgroundColor: "#ffffff00",
-    cursor: "pointer",
-    transform: "scale(0.8)",
-    marginTop: "0",
-    paddingTop: "0",
-}
-
 function Links(e) {
-  // eslint-disable-next-line
-  const {t, i18n} = useTranslation('common');
   return (
     <>
       <div id="copyright">
@@ -44,9 +21,7 @@ function Links(e) {
       </div>
 
       <div id="languagePicker">
-        <p>Choose Language</p>
-        <button id="norwegian" style={norBtn} onClick={() => i18n.changeLanguage('no')}><img src={NorFlag} alt="Norwegian flag" /></button>
-        <button id="english" style={engBtn} onClick={() => i18n.changeLanguage('en')}><img src={EngFlag} alt="English flag" /></button>
+        <Language />
       </div>
 
         <div className="github">
