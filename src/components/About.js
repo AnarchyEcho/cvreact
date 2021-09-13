875c25f488f3f9859ce8f419a6fc7c8d4807e001
kodeandre <andre.kodehode@gmail.com>
2021-09-13 10:13:08 +0200

    v0.3.5

--- src/components/About.js
+++ src/components/About.js
@@ -23,7 +23,7 @@ export default function About() {
             <AboutTitles>Background: </AboutTitles>
             <p>I have a background in hardware and SYSAdmin work with 2 separate companies, one where I fixed and did maintenance on PC's, Laptops and other technical hardware, and one where I maintained and made a webstore for the company, and helped with general IT.</p>
             <AboutTitles>Skills: </AboutTitles>
-            <p>Including coding in React, React Native, HTML, CSS, JavaScript, i also know to some extent: Lua, Python, Java and C#, as I have used them for various game related uses such as mods and addons. I'd say I have a preference for Back-end.</p>
+            <p>Including coding in React, React Native, HTML, CSS, JavaScript, i also know to some extent: Lua, Python, Java and C#, as I have used them for various game related uses such as mods and addons. I'd say I have a preference for Back-end. I've also recently got into MERN stack development.</p>
             <AboutTitles>School: </AboutTitles>
             <p>I graduated from Nordahl Grieg videregående as a Media student, with my main focus being on photography and design.</p>
             <AboutTitles>Contact Me: </AboutTitles>
