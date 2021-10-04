cc53c8a87a7a2d0732c1ea290f0850c58e6cf1f7
KodeAndre <andre.kodehode@gmail.com>
2021-10-04 09:21:01 +0200

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_ini.9d40923e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcvreact=this.webpackJsonpcvreact||[]).push([[60],{647:function(t,n,s){"use strict";function a(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=a,a.displayName="ini",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.9d40923e.chunk.js.map
---
+++ static/js/react-syntax-highlighter_languages_refractor_properties.102a3c7a.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcvreact=this.webpackJsonpcvreact||[]).push([[106],{687:function(t,s,e){"use strict";function n(t){t.languages.properties={comment:/^[ \t]*[#!].*$/m,"attr-value":{pattern:/(^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?: *[=:] *| ))(?:\\(?:\r\n|[\s\S])|[^\\\r\n])+/m,lookbehind:!0},"attr-name":/^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?= *[=:] *| )/m,punctuation:/[=:]/}}t.exports=n,n.displayName="properties",n.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_properties.102a3c7a.chunk.js.map
